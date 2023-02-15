09383f858da007a7b4f766134bfdca80afe28356
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-02-15 17:05:53 +0100

    edit form issues

--- resources/views/grade/form.blade.php
+++ resources/views/grade/form.blade.php
@@ -1,17 +1,28 @@
 <div class="box box-info padding-1">
     <div class="box-body">
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             {{ Form::label('student_id') }}
-            {{ Form::text('student_id', $students, $grade->student_id, ['class' => 'form-control' . ($errors->has('student_id') ? ' is-invalid' : ''), 'placeholder' => 'Student Id']) }}
+            {{ Form::select('student_id', $grade->student_id, ['class' => 'form-control' . ($errors->has('student_id') ? ' is-invalid' : ''), 'placeholder' => 'Student Id']) }}
             {!! $errors->first('student_id', '<div class="invalid-feedback">:message</div>') !!}
         </div>
-        
+
         <div class="form-group">
             {{ Form::label('maths_grade') }}
             {{ Form::text('maths_grade', $grade->maths_grade, ['class' => 'form-control' . ($errors->has('maths_grade') ? ' is-invalid' : ''), 'placeholder' => 'Maths Grade']) }}
             {!! $errors->first('maths_grade', '<div class="invalid-feedback">:message</div>') !!}
+        </div> -->
+
+        <!-- <div class="form-group">
+        {{ Form::label('student_id') }}
+        {{ Form::select('student_id', $students, $grade->student_id, ['class' => 'formcontrol' . ($errors->has('student_id') ? ' is-invalid' : ''), 'placeholder' => 'Student Id']) }}
+        {!! $errors->first('student_id', '<div class="invalid-feedback">:message</div>')!!}
         </div>
+        <div class="form-group">
+        {{ Form::label('maths_grade') }}
+        {{ Form::text('maths_grade', $grade->maths_grade, ['class' => 'form-control' . ($errors->has('maths_grade') ? ' is-invalid' : ''), 'placeholder' => 'Maths Grade']) }}
+        {!! $errors->first('maths_grade', '<div class="invalid-feedback">:message</div>') !!}
+        </div> -->
 
     </div>
     <div class="box-footer mt20">
